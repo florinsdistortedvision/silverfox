60ab9cb1ab7a5ca32382ebcf05c59a9940db2850
★ florin <97384306+florinsdistortedvision@users.noreply.github.com>
2023-12-25 23:35:33 +0200

    Closed Beta 0.2
    To update, remove both user.js and chrome folder and add the new ones in place.
    
    Additions/Fixes:
    
    - Adds titlebars for Linux.
    - Add support for Aero (Windows 7) and buggy support for Aero Basic (hover issue).
    - Silverfox's titlebars behave like Chrome now (when Dark/Light theme - default ones - it will show native titlebars for Win 7 and 8, and if custom themes it will show Chrome's titlebars) - visual bug: when switching to custom themes from default it will show black - restart Firefox and it will fix itself.
    - Fixes Hamburger menu buttons for Windows 7 (8).
    - Fixes Silverfox bookmark text.
    - Fixes About Chrome/Chromium copyright text position for Win 7 and 8.
    - Fixes Nav-Bar's height and border when bookmarks toolbar is disabled.
    - Tab font is now normal instead of bold.
    - In fullscreen, all borders besides Nav-Bar top and bottom or PersonalToolbar bottom has been disabled.
    
    Additions to user.js :
    
    - Disable Pocket section and Sponsored section of homepage
    - Force disable ask where you download to make downloads bar work

--- theme/user.js
+++ theme/user.js
@@ -6,6 +6,7 @@ user_pref("browser.display.windows.non_native_menus", 0); // Minimize the occurr
 user_pref("ui.prefersReducedMotion", 1); // Needed for New Tab button to stick to tabs
 user_pref("browser.tabs.inTitlebar", 1); // Force disable titlebar
 user_pref("browser.uidensity", -1); // Force set normal density
+user_pref("browser.download.useDownloadDir", false); // Forces disabling ask where you save files, needed for Downloads Bar to show
 user_pref("browser.newtabpage.activity-stream.showSponsored", false); // Disable "Sponsored stories" in Pocket section of new tab page
 user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false); // Disable the Pocket section of the new tab page
 user_pref("widget.gtk.native-context-menus", true); // LINUX ONLY - force native GTK3 context menus where possible for more authenticity
